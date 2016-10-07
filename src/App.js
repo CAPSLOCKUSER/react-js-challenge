f2d7dd1bf6cbec74c185e4c9a774530b519f85db
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-10-07 18:48:35 +0200

    material ui sitebuild

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Card} from 'material-ui/Card';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+  container: {
+    width: 400,
+    minHeight: 360,
+    marginTop: 30,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  formBody: {
+    padding: 20,
+  },
+  buttonNext: {
+    float: 'right',
+  },
+  toggle: {
+    marginTop: 30,
+    marginBottom: 30,
+  },
+};
+
+const FormBody = ({ tab }) => (
+  <div style={styles.formBody}>
+    <TextField
+      hintText="Your name"
+      floatingLabelText="Name"
+    /><br />
+    <TextField
+      hintText="Your company"
+      floatingLabelText="Company"
+    /><br />
+    <Toggle
+      label="I agree to the terms of service"
+      style={styles.toggle}
+    />
+    {tab === 'review' ? <RaisedButton label="Back" /> : null}
+    {tab !== 'final' ? <RaisedButton label={tab === 'review' ? 'Submit' : 'Review'} style={styles.buttonNext} /> : null}
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Card style={styles.container}>
+        <Tabs style={styles.tabs}>
+          <Tab label="Form" >
+            <FormBody tab="form" />
+          </Tab>
+          <Tab label="Review" >
+            <FormBody tab="review" />
+          </Tab>
+          <Tab label="Final">
+            <FormBody tab="final" />
+          </Tab>
+        </Tabs>
+      </Card>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#288DEE',
+    accent1Color: '#134294',
+    canvasColor: '#f6f4f5',
+  }
+});
+
+const app = (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>
 );
+
+ReactDOM.render(app, document.getElementById('root'));
